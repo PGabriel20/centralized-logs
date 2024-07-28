719fed7c6fcc9a19522b0b7e5e43be51aecde868
PGabriel20 <bielporreca@gmail.com>
2024-07-28 19:05:36 -0300

    Dockerized application

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,8 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
+Route::get('/hello', function () {
+    return response()->json(['msg' => 'Hello World']);
+});
+
 require __DIR__.'/auth.php';
